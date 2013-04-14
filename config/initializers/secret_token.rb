f45f67527e961d6fe2798498b6ace964968a8f5b
acadavid <acadavid@gmail.com>
2013-04-13 20:14:30 -0500

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Adoptamedellin::Application.config.secret_token = '7c118693af67c889b4e831795a3dceb38dca5730f2d46ca94806911f447bc5e90329a4f64139c7c6fe5677ad9e3d02ae072a7ed1ff5739a2f0ba1bbee46568e8'
