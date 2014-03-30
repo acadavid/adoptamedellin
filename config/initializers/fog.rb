d8cacdd8f4ed3b34a9c49cbc6fb3db80074d4d3d
acadavid <acadavid@gmail.com>
2014-03-30 14:37:31 -0500

    Set ENV vars for aws

--- config/initializers/fog.rb
+++ config/initializers/fog.rb
@@ -1,8 +1,8 @@
 CarrierWave.configure do |config|
   config.fog_credentials = {
     :provider               => 'AWS',
-    :aws_access_key_id      => 'AKIAI3FF47R4CLAFQ7VQ',
-    :aws_secret_access_key  => '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',
+    :aws_access_key_id      => ENV['AWS_ACCESS_KEY'],
+    :aws_secret_access_key  => ENV['AWS_SECRET_ACCESS_KEY']
   }
   config.fog_directory  = 'adoptamedellin'
   config.fog_public     = false
