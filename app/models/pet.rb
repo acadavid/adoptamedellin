23081227746c2b898be3bba79f966de96879f1ab
Andres Martinez Gutierrez <amartinez0000@gmail.com>
2014-10-29 12:11:29 -0500

    Server side validation for gender and kind of animal values

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -2,11 +2,14 @@ class Pet < ActiveRecord::Base
   attr_accessible :age, :animal, :breed, :color, :gender, :location, :municipality, :marks, :name, :notes, :size, :story, :pictures_attributes, :pet_contact_attributes
 
   validates :animal, :presence => true
+  validates :gender, :presence => true
   validates :name, :presence => true
   validates :location, :presence => true
   validates :municipality, :presence => true
-  validate :at_least_one_picture
   validates :pet_contact, :presence => true
+  validate :kinf_of_animal
+  validate :gender_of_animal
+  validate :at_least_one_picture
 
   belongs_to :owner, :polymorphic => :true
   has_many :pictures, :dependent => :destroy
@@ -15,8 +18,16 @@ class Pet < ActiveRecord::Base
 
   accepts_nested_attributes_for :pictures, :pet_contact
 
-  def self.valid_animals
-    ["perro", "gato"]
+  def kinf_of_animal
+    unless self.animal.nil?
+      errors.add(:animal, "es inválido") unless ["Perro", "Gato"].include?(self.animal)
+    end
+  end
+
+  def gender_of_animal
+    unless self.gender.nil?
+      errors.add(:gender, "es inválido") unless ["Macho", "Hembra"].include?(self.gender)
+    end
   end
 
   def at_least_one_picture
